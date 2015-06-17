6c8a8837a10c787eb2d3d80014cd9a5ed14f0360
Jer Lance <jer.lance@perficient.com>
2015-06-17 16:40:23 +0000

    Adds exercise 4.2.1

--- app/code/Perficient/Training381/Controller/Layout/Onecolumn.php
+++ app/code/Perficient/Training381/Controller/Layout/Onecolumn.php
@@ -1,19 +1,36 @@
 <?php
 namespace Perficient\Training381\Controller\Layout;
 
-use \Magento\Framework\App\Action\Action;
+class Onecolumn extends \Magento\Framework\App\Action\Action {
 
-class Onecolumn extends Action {
+    private $_resultPage;
+    private $_model;
+    private $_collection;
 
-    private $resultPage;
-
-    public function __construct(\Magento\Framework\App\Action\Context $context, \Magento\Framework\View\Result\Page $resultPage) {
-        $this->resultPage = $resultPage;
+    public function __construct(\Magento\Framework\App\Action\Context $context,
+                                \Magento\Framework\View\Result\Page $resultPage )
+    {
+        $this->_resultPage = $resultPage;
         parent::__construct($context);
     }
 
     public function execute() {
-        $this->resultPage->initLayout();
-        return $this->resultPage;
+        // Add a model & resource
+        $this->_model = $this->_objectManager->get('Magento\Catalog\Model\Product');
+        $this->_model->load(2045);
+        $output = $this->_model->getSku() . "<br />";
+
+        // Add a collection
+        $this->_collection = $this->_objectManager->create('Magento\Cms\Model\Resource\Block\Collection');
+        $this->_collection->addOrder('title', \Magento\Cms\Model\Resource\Block\Collection::SORT_ORDER_ASC);
+        $this->_collection->addFieldToFilter('title', array('like' => '%left%'));
+
+        foreach ($this->_collection as $cmsBlock) {
+            $output .= $cmsBlock->getTitle() . "<br />";
+        }
+
+        $this->_resultPage->initLayout();
+        $this->getResponse()->appendBody($output);
+        return $this->_resultPage;
     }
 }
