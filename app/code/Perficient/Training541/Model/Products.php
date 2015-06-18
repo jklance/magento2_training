b9488e563bd6b06892528e5cfe5d6b8ce157252f
Jer Lance <jer.lance@perficient.com>
2015-06-18 19:43:12 +0000

    Broke exercise 5.4.1 into cleaner methods

--- app/code/Perficient/Training541/Model/Products.php
+++ app/code/Perficient/Training541/Model/Products.php
@@ -8,11 +8,12 @@ class Products {
     private $_filterBuilder;
     private $_sortOrderBuilder;
 
-    public function __construct(\Magento\Catalog\Api\ProductRepositoryInterface $productRepositoryInterface,
-                                \Magento\Framework\Api\SearchCriteriaBuilder    $searchCriteriaBuilder,
-                                \Magento\Framework\Api\FilterBuilder            $filterBuilder,
-                                \Magento\Framework\Api\SortOrderBuilder         $sortOrderBuilder)
-    {
+    public function __construct(
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepositoryInterface,
+        \Magento\Framework\Api\SearchCriteriaBuilder    $searchCriteriaBuilder,
+        \Magento\Framework\Api\FilterBuilder            $filterBuilder,
+        \Magento\Framework\Api\SortOrderBuilder         $sortOrderBuilder
+    ) {
         $this->_productRepositoryInterface  = $productRepositoryInterface;
         $this->_searchCriteriaBuilder       = $searchCriteriaBuilder;
         $this->_filterBuilder               = $filterBuilder;
@@ -20,29 +21,40 @@ class Products {
     }
 
     public function getProducts() {
-        $filters[] = $this->_filterBuilder
-            ->setField('name')->setConditionType('like')->setValue('%short%')
-        ->create();
-        $this->_searchCriteriaBuilder->addFilter($filters);
-
-        $filters[] = $this->_filterBuilder
-            ->setField('price')->setConditionType('lt')->setValue('50')
-        ->create();
-        $this->_searchCriteriaBuilder->addFilter($filters);
-
-        $sort = $this->_sortOrderBuilder
-            ->setField('name')
-            ->setDirection(\Magento\Framework\Api\SearchCriteriaInterface::SORT_DESC)
-        ->create();
-        $this->_searchCriteriaBuilder->addSortOrder($sort);
-
-        $this->_searchCriteriaBuilder->setPageSize(6);
-        $this->_searchCriteriaBuilder->setCurrentPage(1);
+        $this->_addProductFilters();
+        $this->_addProductSort();
+        $this->_addProductPagination();
 
         $searchCriteria = $this->_searchCriteriaBuilder->create();
         $searchResults = $this->_productRepositoryInterface->getList($searchCriteria);
 
         return $searchResults->getItems();
     }
+
+    private function _addProductFilters() {
+        $filters[] = $this->_filterBuilder
+            ->setField('name')->setConditionType('like')->setValue('%short%')
+            ->create();
+        $this->_searchCriteriaBuilder->addFilter($filters);
+
+        $filters[] = $this->_filterBuilder
+            ->setField('price')->setConditionType('lt')->setValue('50')
+            ->create();
+        $this->_searchCriteriaBuilder->addFilter($filters);
+    }
+
+    private function _addProductSort() {
+        $sort = $this->_sortOrderBuilder
+            ->setField('name')
+            ->setDirection(\Magento\Framework\Api\SearchCriteriaInterface::SORT_DESC)
+            ->create();
+        $this->_searchCriteriaBuilder->addSortOrder($sort);
+    }
+
+    private function _addProductPagination() {
+        $this->_searchCriteriaBuilder->setPageSize(6);
+        $this->_searchCriteriaBuilder->setCurrentPage(1);
+    }
+
 
 }
